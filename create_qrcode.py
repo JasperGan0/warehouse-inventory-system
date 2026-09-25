f4979e23fb2661aa71c59a27e0ee5ef614b6a928
JasperGan0 <ganjasper149@gmail.com>
2026-09-25 21:19:16 +0800

    fixed create_qrcode.py's function.

--- create_qrcode.py
+++ create_qrcode.py
@@ -2,12 +2,11 @@ import qrcode
 
 def create_qrcode(item_sku: str):
     qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        version=None,
         box_size=10,
         border=4,
     )
-    qr.add_data('Some data')
+    qr.add_data(item_sku)
     qr.make(fit=True)
 
     img = qr.make_image(fill_color="black", back_color="white")
